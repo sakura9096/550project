49507ee0c1d44a57688ac868c71b84b31701f2d3
Xiandong Wang <saburowang23@gmail.com>
2015-10-25 19:35:00 -0400

    coding started

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
   has_many :active_relationships, class_name: "Followship", foreign_key: "follower_id",
   										dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
