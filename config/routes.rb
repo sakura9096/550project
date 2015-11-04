05c00a89b1d87ec916afa2a9bc61faa178201c0b
Xiandong Wang <saburowang23@gmail.com>
2015-11-03 22:43:04 -0500

    eb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+  root :to => 'pages#home'
+
   resources :genres
-  root 'pages#home'
 
   resources :movies
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  # devise_scope :user do
-  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  # end
+ 
   resources :users, :only => [:show, :index] do
     member do
       get :following, :followers
