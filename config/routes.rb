c2fc1fdad76e1deeffd880287ceba9ae85e05069
Xiandong Wang <saburowang23@gmail.com>
2015-11-08 16:05:56 -0500

    revise routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :movies
 
-  #devise_for(:users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" })
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
  
   resources :users, :only => [:show, :index] do
     member do
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,35 +15,35 @@ end
 
 require 'Set'
 
-res = []
-file = File.readlines("#{Rails.root}/public/TMDBMovieInfo.json").each do |line|
-	data = JSON.parse(line)
-	res.push(data)
-end
+# res = []
+# file = File.readlines("#{Rails.root}/public/TMDBMovieInfo.json").each do |line|
+# 	data = JSON.parse(line)
+# 	res.push(data)
+# end
 
-genre = Set.new
+# genre = Set.new
 
-res.each! do |data|
-	genre = genre.merge(data["genres"])
-end
+# res.each! do |data|
+# 	genre = genre.merge(data["genres"])
+# end
 
-res = []
-file = File.readlines("#{Rails.root}/public/TMDBMovieInfo.json").each do |line|
-	data = JSON.parse(line)
-	res.push(data)
-end
+# res = []
+# file = File.readlines("#{Rails.root}/public/TMDBMovieInfo.json").each do |line|
+# 	data = JSON.parse(line)
+# 	res.push(data)
+# end
 
-res.each! do |data|
-	Movie.create!(title: data["title"], 
-				overview: data["overview"], 
-				poster: data["poster"],
-				tmdb_id: data["id"], 
-				rating: data["userrating"])
-end
+# res.each! do |data|
+# 	Movie.create!(title: data["title"], 
+# 				overview: data["overview"], 
+# 				poster: data["poster"],
+# 				tmdb_id: data["id"], 
+# 				rating: data["userrating"])
+# end
 
-genre.each do |g|
-	Genre.create!(name: g)
-end	
+# genre.each do |g|
+# 	Genre.create!(name: g)
+# end	
 
 res = []
 file = File.readlines("#{Rails.root}/public/TMDBMovieInfo.json").each do |line|
